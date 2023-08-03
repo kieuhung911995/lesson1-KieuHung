36e08fed5799fc742ae884d911dcf5bd30860eb4
kieuhung911995 <kieuhung911995@gmail.com>
2023-08-03 14:58:53 +0700

    reup

---
+++ index.js
@@ -0,0 +1,6 @@
+function openNav(param) {
+  let overlay = document.getElementsByClassName("header__overlay");
+  param.classList.toggle("change");
+  overlay[0].classList.toggle("change");
+  console.log(overlay[0].classList);
+}
